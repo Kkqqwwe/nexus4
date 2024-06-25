527dc887a2fe16ee7c7b15c881529ce061faef1f
Kkqqwwe <154258107+Kkqqwwe@users.noreply.github.com>
2024-06-25 18:20:18 +0000

    ds

---
+++ nexus-project/target/riscv32i-unknown-none-elf/debug/nexus-project.d
@@ -0,0 +1,1 @@
+/workspaces/nexus4/nexus-project/target/riscv32i-unknown-none-elf/debug/nexus-project: /workspaces/nexus4/nexus-project/src/main.rs
---
+++ nexus-project/target/riscv32i-unknown-none-elf/release-unoptimized/nexus-project.d
@@ -0,0 +1,1 @@
+/workspaces/nexus4/nexus-project/target/riscv32i-unknown-none-elf/release-unoptimized/nexus-project: /workspaces/nexus4/nexus-project/src/main.rs
